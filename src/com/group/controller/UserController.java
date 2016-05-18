306ec7e11b773b83368749a4502e77af5f85dcd3
lahiru <lahirupathiranalr@gmail.com>
2016-05-18 10:25:32 +0530

    init

---
+++ src/com/group/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.group.controller;
+
+import com.group.model.User;
+
+/**
+ * Created by lahiru on 5/18/2016.
+ */
+public class UserController {
+    User tempUsr = new User();
+    tempUsr.
+}
---
+++ src/com/group/model/User.java
@@ -0,0 +1,27 @@
+package com.group.model;
+
+import com.group.db.DBConnection;
+/**
+ * Created by lahiru on 5/18/2016.
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
